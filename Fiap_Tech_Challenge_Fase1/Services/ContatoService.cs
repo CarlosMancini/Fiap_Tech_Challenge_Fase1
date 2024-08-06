a28f1078c10878806bdb138c8ba7093a567a365b
levi-melo <levicontat@outlook.com>
2024-08-06 14:03:10 -0300

    add new services

---
+++ Fiap_Tech_Challenge_Fase1/Services/ContatoService.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities;
+using Core.Repository;
+using Core.Services;
+
+
+namespace Fiap_Tech_Challenge_Fase1.Services
+{
+    public class ContatoService(IContatoRepository contatoRepository, IRegiaoRepository regiaoRepository) : Service<Contato>(contatoRepository), IContatoService
+    {
+        private readonly IRegiaoRepository _regiaoRepository = regiaoRepository;
+        private readonly IContatoRepository _contatoRepository = contatoRepository;
+
+        public void Cadastrar (Contato entidade)
+        {
+            int region = Int32.Parse(entidade.ContatoTelefone[..2]);
+            var SelectedRegion = _regiaoRepository.ObterTodos().FirstOrDefault(item => item.RegiaoDdd == region);
+            if (SelectedRegion != null)
+            {
+                Regiao regiao = new()
+                {
+                    RegiaoNome = "nao entendi pq do nome",
+                    RegiaoDdd = region,
+                };
+                _regiaoRepository.Cadastrar(regiao);
+            }
+            base.Cadastrar (entidade);
+        }
+        public IList<Contato> ObterPorRegião(int RegiaoId)
+        {
+            return _contatoRepository.ObterPorRegião(RegiaoId);
+        }
+
+    }
+}
---
+++ Fiap_Tech_Challenge_Fase1/Services/Service.cs
@@ -0,0 +1,40 @@
+﻿using Core.Entities;
+using Core.Repository;
+using Core.Services;
+
+namespace Fiap_Tech_Challenge_Fase1.Services
+{
+    public class Service<T> : IService<T> where T : EntityBase
+    {
+        private readonly IRepository<T> _repository;
+        public Service(IRepository<T> repository) => _repository = repository;
+        public void Alterar(T entidade)
+        {
+            if (entidade.Id == null)
+            {
+                throw new Exception("Sem id enviado");
+            }
+            _repository.Alterar(entidade);
+        }
+
+        public void Cadastrar(T entidade)
+        {
+            _repository.Cadastrar(entidade);
+        }
+
+        public void Deletar(int id)
+        {
+            _repository.Deletar(id);
+        }
+
+        public T ObterPorId(int id)
+        {
+            return _repository.ObterPorId(id);
+        }
+
+        public IList<T> ObterTodos()
+        {
+            return _repository.ObterTodos();
+        }
+    }
+}

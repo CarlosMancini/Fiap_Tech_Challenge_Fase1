9802c5dca0621c73808e735fc81e026c69d17ac1
Carlos Eduardo <carlos_mancini015@outlook.com>
2024-12-07 14:45:06 -0300

    feat(*) -- cria projeto de Cadastro integrando rabbitmq com masstransit

---
+++ Cadastro/Controllers/CadastroController.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cadastro.Controllers
+{
+    [ApiController]
+    [Route("/Cadastro")]
+    public class CadastroController : ControllerBase
+    {
+        private readonly IBus _bus;
+        private readonly IConfiguration _configuration;
+
+        public CadastroController(IBus bus, IConfiguration configuration)
+        {
+            _bus = bus;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CadastrarContato(/*[FromBody] ContatoInputCadastrar contato*/)
+        {
+            var nomeFila = _configuration.GetSection("MassTransit")["NomeFila"] ?? string.Empty;
+            var endpoint = await _bus.GetSendEndpoint(new Uri($"queue:{nomeFila}"));
+
+            var contato = new Contato { ContatoNome = "Cadu", ContatoEmail = "cadu@cadu.com", ContatoTelefone = "61956833569" };
+
+            await endpoint.Send(contato);
+
+            //await _cadastroService.CadastrarContato(contato);
+            return Ok("Contato enviado para a fila.");
+        }
+    }
+}
---
+++ Cadastro/Program.cs
@@ -0,0 +1,46 @@
+using MassTransit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var configuration = builder.Configuration;
+var servidor = configuration.GetSection("MassTransit")["Servidor"] ?? string.Empty;
+var usuario = configuration.GetSection("MassTransit")["Usuario"] ?? string.Empty;
+var senha = configuration.GetSection("MassTransit")["Senha"] ?? string.Empty;
+
+builder.Services.AddMassTransit((x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(servidor, "/", h =>
+        {
+            h.Username(usuario);
+            h.Password(senha);
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+}));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

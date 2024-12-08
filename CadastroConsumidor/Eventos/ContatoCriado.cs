cb34d624b89962c2bb1b714bfff6dc37536de54b
Carlos Eduardo <carlos_mancini015@outlook.com>
2024-12-08 20:25:51 -0300

    feat(*) -- segmenta produtores e consumidores para serviços específicos de cadastro, atualização e exclusão

---
+++ CadastroConsumidor/Eventos/ContatoCriado.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entities;
+using Core.Interfaces.Services;
+using Core.Mensagens;
+using MassTransit;
+
+namespace CadastroConsumidor.Eventos
+{
+    public class ContatoCriado : IConsumer<ContatoCriadoMensagem>
+    {
+        private readonly ICadastroService _cadastroService;
+
+        public ContatoCriado(ICadastroService cadastroService)
+        {
+            _cadastroService = cadastroService;
+        }
+
+        public async Task Consume(ConsumeContext<ContatoCriadoMensagem> context)
+        {
+            try
+            {
+                Contato contato = new Contato
+                {
+                    ContatoNome = context.Message.ContatoNome,
+                    ContatoTelefone = context.Message.ContatoTelefone,
+                    ContatoEmail = context.Message.ContatoEmail
+                };
+
+                await _cadastroService.Cadastrar(contato);
+                Console.WriteLine($"Contato {contato.ContatoNome} cadastrado com sucesso.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao cadastrar contato: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ ExclusaoProdutor/Controllers/ExclusaoController.cs
@@ -0,0 +1,37 @@
+﻿using Core.Mensagens;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExclusaoProdutor.Controllers
+{
+    [ApiController]
+    [Route("/Exclusao")]
+    public class ExclusaoController : ControllerBase
+    {
+        private readonly IBus _bus;
+        private readonly IConfiguration _configuration;
+
+        public ExclusaoController(IBus bus, IConfiguration configuration)
+        {
+            _bus = bus;
+            _configuration = configuration;
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(ContatoExcluidoMensagem input)
+        {
+            try
+            {
+                var nomeFila = _configuration.GetSection("MassTransit")["NomeFila"] ?? string.Empty;
+                var endpoint = await _bus.GetSendEndpoint(new Uri($"queue:{nomeFila}"));
+
+                await endpoint.Send(input);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+    }
+}
